1d87863616d3129c8fceb4fe8815a319289d28ea
songsf <learnerfan@163.com>
2017-01-01 00:28:05 +0800

    spring-boot集成swagge

--- src/main/java/com/sonsf/learn/controller/GreetingController.java
+++ src/main/java/com/sonsf/learn/controller/GreetingController.java
@@ -2,6 +2,7 @@ package com.sonsf.learn.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by songsf on 2016/12/31.
  */
-@RequestMapping("/api/v1.0/greeting")
+@RequestMapping(value = "/api/v1.0/greeting")
 @RestController
 public class GreetingController {
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    @ApiOperation(value = "问候接口",notes = "hello")
-    public JSONObject greet(@RequestParam()String name){
+    @ApiOperation(value = "问候接口",notes = "这个接口通过输入一个名字，返回hell+name信息，内容放在message对象中")
+    public JSONObject greet(@ApiParam(value = "名字")@RequestParam()String name){
         JSONObject response = new JSONObject();
         response.put("message","hello"+"   "+name);
         return response;
